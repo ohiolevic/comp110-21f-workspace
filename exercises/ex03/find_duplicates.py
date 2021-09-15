63dbe4fc53cab33e5c2d0533d96d6f20e8a148a0
Victor Ilevbare <ohiole@live.unc.edu>
2021-09-14 21:41:03 -0400

    Progress on ex03

--- exercises/ex03/find_duplicates.py
+++ exercises/ex03/find_duplicates.py
@@ -1,3 +1,17 @@
 """Finding duplicate letters in a word."""
 
-__author__ = "123456789"
+__author__ = "730234891"
+word: str = input("Enter a word: ")
+
+x: int = 0 
+y: int = 0 
+
+while x < len(word): 
+    i: int = x + 1 
+    while i < len(word): 
+        if word[x] == word[i]: 
+            y = y + 1 
+        i = i + 1
+    x = x + 1
+
+print("Found duplicate: " + str(y > 0))
--- exercises/ex03/happy_trees.py
+++ exercises/ex03/happy_trees.py
@@ -4,3 +4,10 @@ __author__ = "123456789"
 
 # The string constant for the pine tree emoji
 TREE: str = '\U0001F332'
+times: int = int(input("Depth: "))
+
+cycles: int = 1 
+
+while cycles <= times:
+    print(TREE * cycles)
+    cycles = cycles + 1
