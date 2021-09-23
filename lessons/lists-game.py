1e4f41c1acb89b852afb447b7ddc2939f0f90272
Victor Ilevbare <ohiole@live.unc.edu>
2021-09-22 23:12:45 -0400

    Progress on Project

---
+++ lessons/lists-game.py
@@ -0,0 +1,27 @@
+"""Examples of using lists in a simple 'game'. """
+
+from random import randint 
+
+rolls: list[int] = list()
+
+while len(rolls) == 0 or rolls[len(rolls) - 1] != 1: 
+    rolls.append(randint(1, 6))
+
+print(rolls)
+
+
+# Remove an item from the list by its index("pop")
+rolls.pop(len(rolls) - 1)
+print(rolls)
+
+# Sum the values of the rolls!
+i: int = 0 
+sum: int = 0 
+while i < len(rolls): 
+    sum = sum + rolls[i]
+    i = i + 1 
+
+print(f"Total score: {sum}")
+
+# rolls.append(randint(1,6))
+# rolls.append(randint(1,6))
