746ff3b77551d6c07a0880d1fe7de828caf31cf4
Victor Ilevbare <ohiole@live.unc.edu>
2021-10-06 19:21:44 -0400

    Progress on Ex 05

--- exercises/ex05/utils.py
+++ exercises/ex05/utils.py
@@ -1,5 +1,46 @@
 """List utility functions part 2."""
 
-__author__ = "123456789"
+__author__ = "730234891"
 
 # Define your functions below
+
+
+def only_evens(list_1: list[int]) -> list[int]:
+    """Returns list only containing even numbers."""
+    i: int = 0
+    xs: list[int] = []
+    while i < len(list_1): 
+        if list_1[i] % 2 == 0: 
+            xs.append(list_1[i])
+        i += 1
+    return xs
+
+
+def sub(list_1: list[int], start_index: int, end_index: int) -> list[int]:
+    """Starting from the starting index to the end returning a sublist."""
+    if len(list_1) == 0 or start_index > len(list_1) or end_index <= 0:
+        return []
+    if start_index < 0: 
+        start_index = 0 
+    i: int = start_index 
+    if end_index > len(list_1):
+        end_index = len(list_1)
+    set: list[int] = []
+    while i < end_index:
+        set.append(list_1[i])
+        i += 1 
+    return set
+
+
+def concat(list_1: list[int], list_2: list[int]) -> list[int]:
+    """Return list containing all elements in one."""
+    x: int = 0 
+    y: int = 0 
+    xs: list[int] = []
+    while x < len(list_1): 
+        xs.append(list_1[x])
+        x += 1 
+    while y < len(list_2): 
+        xs.append(list_2[y])
+        y += 1 
+    return xs        
--- exercises/ex05/utils_test.py
+++ exercises/ex05/utils_test.py
@@ -1,6 +1,63 @@
 """Unit tests for list utility functions."""
 
 # TODO: Uncomment the below line when ready to write unit tests
-# from exercises.ex05.utils import only_evens, sub, concat
+from exercises.ex05.utils import only_evens, sub, concat
 
-__author__ = "123456789"
+__author__ = "730234891"
+
+
+def test_only_evens_empty() -> None: 
+    """Testing the empty edge case for test_only."""
+    list_1: list[int] = []
+    assert only_evens(list_1) == []
+
+
+def test_only_evens_single_item() -> None: 
+    """Testing a single item for only_evens."""
+    list_1: list[int] = [120]
+    assert only_evens(list_1) == [120]
+
+
+def test_only_evens_multiple_items() -> None: 
+    """Testing multiple items for only_evens."""
+    list_1: list[int] = [30, 35, 40, 45, 50, 55]
+    assert only_evens(list_1) == [30, 40, 50]
+
+
+def test_sub_empty() -> None:
+    """Testing the empty edge case for test_sub."""
+    list_1: list[int] = []
+    assert sub(list_1, 0, 0) == []
+
+
+def test_sub_single_items() -> None: 
+    """Testing a single item for test_sub."""
+    list_1: list[int] = [120]
+    assert sub(list_1, 0, 1) == [120]
+
+
+def test_sub_multiple_items() -> None: 
+    """Testing multiple items for test_sub."""
+    list_1: list[int] = [1, 2, 3]
+    assert sub(list_1, 1, 3) == [2, 3]
+
+
+def test_concat_empty() -> None: 
+    """Testing the empty edge case for test_concat."""
+    list_1: list[int] = []
+    list_2: list[int] = []
+    assert concat(list_1, list_2) == []
+
+
+def test_concat_single_item() -> None: 
+    """Testing a single item for test_concat."""
+    list_1: list[int] = [120]
+    list_2: list[int] = [10]
+    assert concat(list_1, list_2) == [120, 10]
+
+
+def test_concat_multiple_items() -> None: 
+    """Testing multiple items for test_concat."""
+    list_1: list[int] = [30, 40, 50]
+    list_2: list[int] = [60, 70, 80]
+    assert concat(list_1, list_2) == [30, 40, 50, 60, 70, 80]
