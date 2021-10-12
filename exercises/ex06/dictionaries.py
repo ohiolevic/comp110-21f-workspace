1f4f90a4f1f6893ce349943a46d2071c38af2dc3
Victor Ilevbare <ohiole@live.unc.edu>
2021-10-12 16:07:56 -0400

    Progress on EX06

--- exercises/ex06/dictionaries.py
+++ exercises/ex06/dictionaries.py
@@ -1,5 +1,48 @@
 """Practice with dictionaries."""
 
-__author__ = "123456789"
+__author__ = "730234891"
 
 # Define your functions below
+
+
+def invert(input_one: dict[str, str]) -> dict[str, str]: 
+    """Inverting the key and the value of a dictionary."""
+    input_one = dict()
+    for x in input_one.keys():
+        for y in input_one.keys():
+            if input_one[x] == input_one[y] and x != y:
+                raise KeyError("There is somthing wrong.")
+    for i in input_one.keys():
+        input_one[input_one[i]] = i 
+        input_one.pop(i)
+    return input_one
+
+
+def favorite_color(input_one: dict[str, str]) -> str:
+    """Common color of the dict."""
+    new_input: dict[str, int] = {}
+    max: int = 0 
+    colour: str = ""
+    for x in input_one:
+        if input_one[x] in new_input:
+            new_input[input_one[x]] += 1
+        else:
+            new_input[input_one[x]] = 1
+    for item in new_input: 
+        if new_input[item] > max: 
+            colour = item
+            max = new_input[item]
+    print(max)
+    return colour
+
+
+def count(input_one: list[str]) -> dict[str, int]:
+    """Counts the times a value is repeated."""
+    xs = dict() 
+    for x in input_one:
+        xs[x] = 0 
+    for x in xs.keys():
+        for y in input_one:
+            if list(y) == x:
+                xs[x] = xs[x] + 1 
+    return xs 
--- exercises/ex06/dictionaries_test.py
+++ exercises/ex06/dictionaries_test.py
@@ -1,6 +1,60 @@
 """Unit tests for dictionary functions."""
 
 # TODO: Uncomment the below line when ready to write unit tests
-# from exercises.ex06.dictionaries import invert, favorite_color, count
+from exercises.ex06.dictionaries import invert, favorite_color, count
 
 __author__ = "123456789"
+
+
+def invert_one() -> None:
+    """Test empty Invert."""
+    test_invert: dict[str, str] = {'Will': 'Victor', 'Emma': 'Phoebe'}
+    assert invert(test_invert) == KeyError
+
+
+def invert_two() -> None:
+    """Test for invert_one."""
+    test_invert: dict[str, str] = {'a': 'x', 'b': 'y', 'c': 'z'}
+    assert invert(test_invert) == {'x': 'a', 'y': 'b', 'z': 'c'}
+
+
+def invert_three() -> None: 
+    """Test for invert 2."""
+    test_invert: dict[str, str] = {'Nigeria': 'France'}
+    assert invert(test_invert) == {'France': 'Nigeria'}
+
+
+def favorite_color_one() -> None: 
+    """Test for favorite color one."""
+    test_dict: dict[str, str] = {}
+    assert favorite_color(test_dict) == ""
+
+
+def favorite_color_two() -> None:
+    """Test for favorite color two."""
+    test_dict: dict[str, str] = {"Victor": "Orange"}
+    assert favorite_color(test_dict) == "Orange"
+
+
+def favorite_color_three() -> None: 
+    """Test for favorite color three."""
+    test_dict: dict[str, str] = {"Victor": "Orange", "Emma": "Blue"}
+    assert favorite_color(test_dict) == "Orange"
+
+
+def count_one() -> None:
+    """Test for empty count one."""
+    test_str: list[str] = ['Evelyn', 'Eunice', 'Emma']
+    assert count(test_str) == ["Emma: 2, Eunice: 1"]
+
+
+def count_two() -> None: 
+    """Test for count two."""
+    test_str: list[str] = ['Victor', 'Eunice', 'Emma']
+    assert count(test_str) == ["Victor: 1, Eunice: 1, Emma: 1"]
+
+
+def count_three() -> None: 
+    """Test for count three."""
+    test_str: list[str] = ['Eunice', 'Emma', 'Eunice']
+    assert count(test_str) == ["Eunice; 2, Emma: 1"]
